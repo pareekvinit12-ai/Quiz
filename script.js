957705cf591deacce0247ff8b091150d3984e127
pareekvinit12-ai <pareekvinit12@gmail.com>
2025-12-10 15:00:49 +0530

    updated

--- script.js
+++ script.js
@@ -9,24 +9,31 @@ let data = [
     q: "What is the capital of China?",
     options: ["beijing", "xinjiang", "shanghai", "hungsung"],
     a: "beijing",
+     image: false
+
   },
   {
     q: "What is the capital of Brazil?",
-    options: ["sao paulo", "brasilia", "sao paulo", "brasilia"],
+    options: ["sao paulo", "brasilia", "sao paulo", "rbasilia"],
     a: "brasilia",
+     image: false
+
   },
   {
     q: "What is the capital of Japan?",
     options: ["tokyo", "osaka", "kyoto", "hiroshima"],
     a: "tokyo",
+     image: false
   },
   {
-    q: "What is the capital of India?",
-    options: ["mumbai", "delhi", "kolkata", "chennai"],
-    a: "delhi",
-  },
+    q: "Where is this monument situated?",
+    imagelink:
+      "https://i.pinimg.com/1200x/f8/c4/65/f8c4655ef6ef16f307438e7c78956e8d.jpg",
+    a: "Agra",
+    options: ["Agra", "Jaipur", "New Delhi", "Udaipur"],
+    image: true
+  }
 ];
-
 let timer = 5;
 let questionNumber = 0;
 let score = 0;
@@ -34,12 +41,12 @@ let score = 0;
 printquesandoption();
 start();
 
-function start() {
+function start(){
   timerDiv.innerHTML = timer;
   let interval = setInterval(() => {
     timer--;
     if (timer === 0) {
-      if (questionNumber === data.length - 1) {
+      if (questionNumber === data.length-1) {
         clearInterval(interval);
 
         quiz.style.display = "none";
@@ -56,10 +63,26 @@ function start() {
     }
   }, 1000);
 }
-
 function printquesandoption() {
-  questionDiv.innerHTML = data[questionNumber].q;
-  optionDiv.forEach((e, index) => {
+  if(!data[questionNumber].image){
+    questionDiv.innerHTML = data[questionNumber].q;
+  }
+  else{
+    questionDiv.innerHTML= "";
+    const span = document.createElement("span");
+    span.innerText = data[questionNumber].q;
+
+    const img = document.createElement("img");
+    img.src = data[questionNumber].imagelink;
+    img.alt = data[questionNumber].question;
+
+    questionDiv.append(span, img);
+
+  }
+
+
+
+optionDiv.forEach((e, index) => {
     e.innerHTML = data[questionNumber].options[index];
   });
 }
@@ -69,7 +92,6 @@ optionDiv.forEach((opt, index) => {
     // console.log(data[questionNumber].a);
 
     if (opt.innerText === data[questionNumber].a) {
-      // console.log("hhkh");
       opt.style.backgroundColor = "green";
       score++;
     } else {
@@ -83,15 +105,22 @@ optionDiv.forEach((opt, index) => {
     }
   });
 });
-
 function resetOptions() {
   optionDiv.forEach((opt) => (opt.style.backgroundColor = "#fff"));
 }
 
 function displayScore() {
   const para = document.createElement("p");
-  para.innerText = `Your Score is ${score} out of ${data.length}`;
+  if(score>=3){
+  para.innerText = ` Congratulations Your Score is ${score} out of ${data.length}`;
+    
+  }
+  else{
+  para.innerText = ` bad Your Score is ${score} out of ${data.length}`;
+
+  }
   document.querySelector("#box").append(para);
+
 }
 
 button.addEventListener("click", () => {
